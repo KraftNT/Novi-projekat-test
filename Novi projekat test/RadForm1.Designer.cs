4768f09aa76caa8d2247d63e8f355c3a1c5c27a3
Nikola Lalevic <nikola.lalevic@kraftnt.com>
2018-10-25 13:05:15 +0200

    dd
    Test

--- Novi projekat test/RadForm1.Designer.cs
+++ Novi projekat test/RadForm1.Designer.cs
@@ -35,7 +35,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 413);
             this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
             this.Text = "RadForm1";
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
