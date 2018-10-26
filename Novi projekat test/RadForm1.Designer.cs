29a671a87b0626af4319c5e900724848bd667216
Danilo Popovic <37156466+dachoux@users.noreply.github.com>
2018-10-26 09:37:29 +0200

    AAA
    AAA

--- Novi projekat test/RadForm1.Designer.cs
+++ Novi projekat test/RadForm1.Designer.cs
@@ -28,25 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radTextBox3 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel4 = new Telerik.WinControls.UI.RadLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.Location = new System.Drawing.Point(66, 13);
+            this.radTextBox1.Name = "radTextBox1";
+            // 
+            // 
+            // 
+            this.radTextBox1.RootElement.AccessibleDescription = null;
+            this.radTextBox1.RootElement.AccessibleName = null;
+            this.radTextBox1.RootElement.Text = null;
+            this.radTextBox1.Size = new System.Drawing.Size(212, 20);
+            this.radTextBox1.TabIndex = 0;
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(11, 14);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(52, 18);
+            this.radLabel1.TabIndex = 1;
+            this.radLabel1.Text = "Komitent";
+            this.radLabel1.ThemeName = "ControlDefault";
+            // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Location = new System.Drawing.Point(66, 37);
+            this.radTextBox2.Name = "radTextBox2";
+            // 
+            // 
+            // 
+            this.radTextBox2.RootElement.Text = "";
+            this.radTextBox2.Size = new System.Drawing.Size(79, 20);
+            this.radTextBox2.TabIndex = 3;
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(23, 38);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(40, 18);
+            this.radLabel2.TabIndex = 4;
+            this.radLabel2.Text = "Datum";
+            this.radLabel2.ThemeName = "ControlDefault";
+            // 
+            // radTextBox3
+            // 
+            this.radTextBox3.Enabled = false;
+            this.radTextBox3.Location = new System.Drawing.Point(199, 38);
+            this.radTextBox3.Name = "radTextBox3";
+            // 
+            // 
+            // 
+            this.radTextBox3.RootElement.AccessibleDescription = null;
+            this.radTextBox3.RootElement.AccessibleName = null;
+            this.radTextBox3.RootElement.Enabled = false;
+            this.radTextBox3.RootElement.Text = null;
+            this.radTextBox3.Size = new System.Drawing.Size(79, 20);
+            this.radTextBox3.TabIndex = 5;
+            // 
+            // radLabel4
+            // 
+            this.radLabel4.Location = new System.Drawing.Point(161, 38);
+            this.radLabel4.Name = "radLabel4";
+            this.radLabel4.Size = new System.Drawing.Size(32, 18);
+            this.radLabel4.TabIndex = 7;
+            this.radLabel4.Text = "Iznos";
+            this.radLabel4.ThemeName = "ControlDefault";
             // 
             // GlavnaForma
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 376);
+            this.ClientSize = new System.Drawing.Size(817, 443);
+            this.Controls.Add(this.radLabel4);
+            this.Controls.Add(this.radTextBox3);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radTextBox2);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radTextBox1);
             this.Name = "GlavnaForma";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
             this.Text = "RadForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadTextBox radTextBox2;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadTextBox radTextBox3;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
     }
 }
--- Novi projekat test/RadForm1.cs
+++ Novi projekat test/RadForm1.cs
@@ -15,5 +15,15 @@ namespace Novi_projekat_test
         {
             InitializeComponent();
         }
+
+        private void radLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
